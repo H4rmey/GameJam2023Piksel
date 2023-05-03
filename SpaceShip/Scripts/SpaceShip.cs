cd5cc2f8ad2ac943b9035382b1c04817f873b6d0
H4rmey <meyer@pop-os.localdomain>
2023-05-03 22:19:41 +0200

    haha spaceship go vroem

--- SpaceShip/Scripts/SpaceShip.cs
+++ SpaceShip/Scripts/SpaceShip.cs
@@ -12,17 +12,32 @@ public partial class SpaceShip : Node3D
     [Export]
 	public NodePath pathBetweenPoints;
     [Export]
-	public double speed;
+	public double speedToEnd = 5;
     [Export]
-	public int nof_passes;
+	public double speedToCow = 3;
+    [Export]
+	public double speedToBetween = 0.5f;
+
+    [Export]
+	public double holdTimeCow = 5;
+    [Export]
+	public double holdTimeEnd = 3;
+    [Export]
+	public double holdTimeBetween = 0.5f;
+    [Export]
+	public float cowHoverBelow = 1;
 	
-	private int currentNodeIndex;
 	private Vector3 nextPosition;
+	private int currentNodeIndex;
+	private Node3D currentTarget;
 
     private CharacterBody3D player;
     private Array<Node3D> cows = new Array<Node3D>();
+    private Array<Node3D> cowsOriginal = new Array<Node3D>();
     private Array<Node3D> endPoints = new Array<Node3D>();
     private Array<Node3D> betweenPoints = new Array<Node3D>();
+
+	private Tween tween; 
 
     public override void _Ready()
     {
@@ -30,6 +45,7 @@ public partial class SpaceShip : Node3D
 		for (int i = 0; i < GetNode<Node3D>(pathCows).GetChildCount(); i++) 
 		{
         	cows.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
+        	cowsOriginal.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
 		}
 		for (int i = 0; i < GetNode<Node3D>(pathEndPoints).GetChildCount(); i++) {
         	endPoints.Add(GetNode<Node3D>(pathEndPoints).GetChild<Node3D>(i));
@@ -38,45 +54,58 @@ public partial class SpaceShip : Node3D
 		{
         	betweenPoints.Add(GetNode<Node3D>(pathBetweenPoints).GetChild<Node3D>(i));
 		}
-		
-		CalculateNextEndPoint();
+		this.GlobalPosition = endPoints[0].GlobalPosition;
 
-		Tween tween = GetTree().CreateTween();
-		tween.TweenProperty(this, "position", nextPosition, 5.0f);
-		tween.SetTrans(Tween.TransitionType.Quart);
+		tween = GetTree().CreateTween();
+		
+		GetCow();
     }
+	
 
     public override void _Process(double delta)
     {
-		LookAtPlayer();
+		GD.Print(this.GlobalPosition);
     }
 	
-	private void LookAtPlayer()
+	private void GetCow()
 	{
-        if (player == null) {
-            GD.Print("Error, player not found for cow: ", this);
-            return; 
-        }
-        this.LookAt(player.GlobalPosition, Vector3.Up);
+		//quickly move between points
+		tween.SetLoops(1);
+		tween.SetTrans(Tween.TransitionType.Quart);
+		CalculateNextEndPoint(betweenPoints);
+		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToBetween);
+		tween.TweenInterval(holdTimeBetween);
+		CalculateNextEndPoint(betweenPoints);
+		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToBetween);
+		tween.TweenInterval(holdTimeBetween);
+		CalculateNextEndPoint(betweenPoints);
+		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToBetween);
+		tween.TweenInterval(holdTimeBetween);
+		//move to cow
+		CalculateNextEndPoint(cows);
+		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToCow);
+		tween.TweenProperty(currentTarget, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y-cowHoverBelow, nextPosition.Z), holdTimeCow);
+		tween.TweenInterval(holdTimeCow/3.0f);
+		//move to the endpoint/homebase
+		CalculateNextEndPoint(endPoints);
+		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToEnd);
+		tween.TweenInterval(holdTimeEnd);
 	}
 
-	private void CalculateNextEndPoint()
+	private void CalculateNextEndPoint(Array<Node3D> nodes)
 	{
 		// Generate the next index
 		RandomNumberGenerator random = new RandomNumberGenerator();
-		int nextIndex = random.RandiRange(0, endPoints.Count-1);
+		int nextIndex = random.RandiRange(0, nodes.Count-1);
 
 		if (currentNodeIndex == nextIndex )
 		{
 			int m = random.RandiRange(0, 1);
-			nextIndex = (m == 0) ? (nextIndex-1)%endPoints.Count : (nextIndex+1)%endPoints.Count;
+			nextIndex = (m == 0) ? (nextIndex-1)%nodes.Count : (nextIndex+1)%nodes.Count;
 		}
-		
-		nextPosition = endPoints[nextIndex].GlobalPosition;
-	}
 
-	private void moveToCow()
-	{
-		
+		currentNodeIndex = nextIndex;	
+		currentTarget = nodes[nextIndex];
+		nextPosition = nodes[nextIndex].GlobalPosition;
 	}
 }
