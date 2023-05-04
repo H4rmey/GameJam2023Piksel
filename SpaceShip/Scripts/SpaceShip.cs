f3da380f41e7db87283da40e624b86bc34022a07
H4rmey <meyer@pop-os.localdomain>
2023-05-04 19:34:40 +0200

    cows get abducted, ownoooo, so sad...

--- SpaceShip/Scripts/SpaceShip.cs
+++ SpaceShip/Scripts/SpaceShip.cs
@@ -30,6 +30,7 @@ public partial class SpaceShip : Node3D
 	private Vector3 nextPosition;
 	private int currentNodeIndex;
 	private Node3D currentTarget;
+	private Node3D cowTarget;
 
     private CharacterBody3D player;
     private Array<Node3D> cows = new Array<Node3D>();
@@ -37,37 +38,33 @@ public partial class SpaceShip : Node3D
     private Array<Node3D> endPoints = new Array<Node3D>();
     private Array<Node3D> betweenPoints = new Array<Node3D>();
 
-	private Tween tween; 
+	public Tween tween; 
 
     public override void _Ready()
     {
         player = GetNode<CharacterBody3D>(pathPlayer);
-		for (int i = 0; i < GetNode<Node3D>(pathCows).GetChildCount(); i++) 
-		{
-        	cows.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
-        	cowsOriginal.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
-		}
-		for (int i = 0; i < GetNode<Node3D>(pathEndPoints).GetChildCount(); i++) {
-			endPoints.Add(GetNode<Node3D>(pathEndPoints).GetChild<Node3D>(i));
-		}
-		for (int i = 0; i < GetNode<Node3D>(pathBetweenPoints).GetChildCount(); i++) 
-		{
-			betweenPoints.Add(GetNode<Node3D>(pathBetweenPoints).GetChild<Node3D>(i));
-		}
+
+		InitMovePoints();
+
 		this.GlobalPosition = endPoints[0].GlobalPosition;
 
 		tween = GetTree().CreateTween();
+		tween.Finished += OnTweenFinished;
 		
-		GetCow();
+		AbductCow();
     }
 	
 
     public override void _Process(double delta)
     {
-		GD.Print(this.GlobalPosition);
     }
-	
-	private void GetCow()
+
+	private void OnTweenFinished()
+	{
+		GD.Print("Tween has finished :D");
+	}
+
+	private void AbductCow()
 	{
 		//quickly move between points
 		tween.SetLoops(1);
@@ -81,15 +78,21 @@ public partial class SpaceShip : Node3D
 		CalculateNextEndPoint(betweenPoints);
 		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToBetween);
 		tween.TweenInterval(holdTimeBetween);
-		//move to cow
+		//
+		//	move to cow
 		CalculateNextEndPoint(cows);
 		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToCow);
 		tween.TweenProperty(currentTarget, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y-cowHoverBelow, nextPosition.Z), holdTimeCow);
 		tween.TweenInterval(holdTimeCow/3.0f);
-		//move to the endpoint/homebase
+		//
+		//	move to the endpoint/homebase
+		cowTarget = currentTarget;
 		CalculateNextEndPoint(endPoints);
 		tween.TweenProperty(this, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y, nextPosition.Z), speedToEnd);
+		//move cow as well
+		tween.Parallel().TweenProperty(cowTarget, "position", new Vector3(nextPosition.X, this.GlobalPosition.Y-cowHoverBelow, nextPosition.Z), speedToEnd);
 		tween.TweenInterval(holdTimeEnd);
+		tween.TweenCallback(Callable.From(() => cows.Remove(cowTarget)));
 	}
 
 	private void CalculateNextEndPoint(Array<Node3D> nodes)
@@ -108,4 +111,30 @@ public partial class SpaceShip : Node3D
 		currentTarget = nodes[nextIndex];
 		nextPosition = nodes[nextIndex].GlobalPosition;
 	}
+
+	private void InitMovePoints()
+	{
+		for (int i = 0; i < GetNode<Node3D>(pathCows).GetChildCount(); i++) 
+		{
+        	cows.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
+        	cowsOriginal.Add(GetNode<Node3D>(pathCows).GetChild<Node3D>(i));
+			SetCowScript(i);
+		}
+		for (int i = 0; i < GetNode<Node3D>(pathEndPoints).GetChildCount(); i++) {
+			endPoints.Add(GetNode<Node3D>(pathEndPoints).GetChild<Node3D>(i));
+		}
+		for (int i = 0; i < GetNode<Node3D>(pathBetweenPoints).GetChildCount(); i++) 
+		{
+			betweenPoints.Add(GetNode<Node3D>(pathBetweenPoints).GetChild<Node3D>(i));
+		}
+	}
+
+	private void SetCowScript(int i)
+	{
+			//set cow script
+			Cow cowScript = cows[i] as Cow;
+			cowScript.spaceShipScript = this as SpaceShip;
+			cowScript.spaceShip = this;
+	}
+	
 }
